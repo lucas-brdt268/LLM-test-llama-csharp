4c5177b1f13c3d393b642489239e1bad74755880
lucas-brdt268 <lucas.brdt268@gmail.com>
2025-09-20 17:17:45 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace LLM_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to your C# project!");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
